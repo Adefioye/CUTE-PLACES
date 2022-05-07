b5dd77a04f66aa31d91be8cca3ee8742afe26386
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-05-07 22:10:49 +0100

    Removed /build from .gitignore

--- server/index.js
+++ server/index.js
@@ -34,15 +34,17 @@ app.use("/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../app/client/build"));
+  const __dirname = path.resolve();
+
+  app.use(express.static("app/client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "app", "client", "build", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "app", "client", "build", "index.html"));
   });
 }
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
+
+// Working on fixing connectionto database
